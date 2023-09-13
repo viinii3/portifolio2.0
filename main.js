2230d7d527eaccf43acda50e5b794623c8f1c1d8
Marcos Vinicius <101298473+viinii3@users.noreply.github.com>
2023-09-13 18:22:00 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    $("#menu-hamburguer").click(function(){
+        $("#menu").slideToggle()
+    })
+
+
+    $("#btn1").click(function(){
+        $(".skill2").slideToggle()
+    })
+
+    $("#btn2").click(function(){
+        $(".projetos2").slideToggle()
+    })
+})
